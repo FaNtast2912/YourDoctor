cf939b6c68eed24cc70ec0c9422654899bb5678e
FaNtast2912 <z_maks@inbox.ru>
2024-12-02 20:55:46 +0300

    Docktors Card Screen added, Price Details Screen added, on going Doctors Card Cell

---
+++ YourDoctor/DocktorsCardCell/DocktorsRating.swift
@@ -0,0 +1,41 @@
+//
+//  DocktorsRating.swift
+//  YourDoctor
+//
+//  Created by Maksim Zakharov on 02.12.2024.
+//
+
+import SwiftUI
+
+struct DocktorsRating: View {
+    var rating: Int
+    var body: some View {
+        HStack(spacing: 2.4) {
+            Image(systemName: "star.fill")
+                .resizable()
+                .foregroundColor(rating >= 1 ? Color("yourDoctor_pink") : Color("yourDoctor_DarkGrey"))
+                .frame(width: 12,height: 12.0)
+            Image(systemName: "star.fill")
+                .resizable()
+                .foregroundColor(rating >= 2 ? Color("yourDoctor_pink") : Color("yourDoctor_DarkGrey"))
+                .frame(width: 12,height: 12.0)
+            Image(systemName: "star.fill")
+                .resizable()
+                .foregroundColor(rating >= 3 ? Color("yourDoctor_pink") : Color("yourDoctor_DarkGrey"))
+                .frame(width: 12,height: 12.0)
+            Image(systemName: "star.fill")
+                .resizable()
+                .foregroundColor(rating >= 4 ? Color("yourDoctor_pink") : Color("yourDoctor_DarkGrey"))
+                .frame(width: 12,height: 12.0)
+            Image(systemName: "star.fill")
+                .resizable()
+                .foregroundColor(rating >= 5 ? Color("yourDoctor_pink") : Color("yourDoctor_DarkGrey"))
+                .frame(width: 12,height: 12.0)
+        }
+        .frame(width: 69.6,height: 12.0)
+    }
+}
+
+#Preview {
+    DocktorsRating(rating: 2)
+}
--- YourDoctor/DocktorsCardCell/DoctorsCardCell.swift
+++ YourDoctor/DocktorsCardCell/DoctorsCardCell.swift
@@ -10,27 +10,44 @@ import SwiftUI
 struct DoctorsCardCell: View {
     var secondName: String
     var fullName: String
-    
+    var rating: Int
     var body: some View {
         VStack {
             VStack {
                 VStack(spacing: 15) {
                     HStack(alignment: .top, spacing: 16.0) {
                         Image("DoctorsPick")
-                        VStack(alignment: .leading) {
-                            Text(secondName)
-                            Text(fullName)
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            VStack(alignment: .leading) {
+                                Text(secondName)
+                                Text(fullName)
+                            }
+                            .bold()
+                            .font(.system(size: 24))
+                            
+                            DocktorsRating(rating: rating)
+                            
+                            Text("Педиатр" + "・" + "стаж 9 лет")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color("yourDoctor_DarkGrey"))
+                            
+                            Text("от 400₽")
+                                .bold()
+                                .font(.system(size: 24))
                         }
-                        .bold()
-                        .font(.system(size: 24))
+                        
                         Image(systemName: "heart")
+                            .resizable()
                             .foregroundColor(Color("yourDoctor_Silver"))
+                            .frame(width: 20, height: 17.16)
+                        
                     }
+                    
                     AppointmentButton(tittle: "Записаться", isAvailable: true, size: CGSize(width: 311, height: 47))
                 }
-                
-                .frame(height: 188)
+
             }
+            .frame(height: 224)
             .padding([.leading, .bottom, .trailing], 16.0)
             .padding(.top, 20)
         }
@@ -39,5 +56,5 @@ struct DoctorsCardCell: View {
 }
 
 #Preview {
-    DoctorsCardCell(secondName: "Осташков", fullName: "Кирилл Вячеславович")
+    DoctorsCardCell(secondName: "Осташков", fullName: "Кирилл Вячеславович", rating: 3)
 }
